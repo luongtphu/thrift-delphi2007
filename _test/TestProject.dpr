82b263929691b14552b2fbac9ea48eebb8ffcceb
luongtphu <luongtphu@gmail.com>
2013-11-25 11:34:59 +0700

    Update project

--- _test/TestProject.dpr
+++ _test/TestProject.dpr
@@ -2,17 +2,31 @@ program TestProject;
 
 {$APPTYPE CONSOLE}
 
-{%File 'uDefine.inc'}
+
+
+{%File 'delphi2007Lib\uDefine.inc'}
 
 uses
   SysUtils,
-  Thrift.Protocol in 'Thrift.Protocol.pas',
-  Thrift.Utils in 'Thrift.Utils.pas',
-  Thrift.Stream in 'Thrift.Stream.pas',
-  Thrift.Transport in 'Thrift.Transport.pas',
-  uStringBuilder in 'uStringBuilder.pas',
-  uRTLConsts in 'uRTLConsts.pas',
-  uSysUtils in 'uSysUtils.pas';
+  Thrift.Utils in 'delphi2007Lib\Thrift.Utils.pas',
+  uRTLConsts in 'delphi2007Lib\uRTLConsts.pas',
+  uStringBuilder in 'delphi2007Lib\uStringBuilder.pas',
+  uSysUtils in 'delphi2007Lib\uSysUtils.pas',
+  trialtest in 'gen-delphi7\trialtest.pas',
+  Thrift.Collections in 'delphi2007Lib\Thrift.Collections.pas',
+  Thrift.Console in 'delphi2007Lib\Thrift.Console.pas',
+  Thrift in 'delphi2007Lib\Thrift.pas',
+  Thrift.Processor.Multiplex in 'delphi2007Lib\Thrift.Processor.Multiplex.pas',
+  Thrift.Protocol.JSON in 'delphi2007Lib\Thrift.Protocol.JSON.pas',
+  Thrift.Protocol.Multiplex in 'delphi2007Lib\Thrift.Protocol.Multiplex.pas',
+  Thrift.Protocol in 'delphi2007Lib\Thrift.Protocol.pas',
+  Thrift.Serializer in 'delphi2007Lib\Thrift.Serializer.pas',
+  Thrift.Server in 'delphi2007Lib\Thrift.Server.pas',
+  Thrift.Stream in 'delphi2007Lib\Thrift.Stream.pas',
+  Thrift.Transport in 'delphi2007Lib\Thrift.Transport.pas',
+  Thrift.Transport.Pipes in 'delphi2007Lib\Thrift.Transport.Pipes.pas',
+  Thrift.TypeRegistry in 'delphi2007Lib\Thrift.TypeRegistry.pas',
+  uCollections in 'delphi2007Lib\uCollections.pas';
 
 begin
   try
