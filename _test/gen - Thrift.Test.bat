d4823f24e4f3e0343dd9facb2c14376e991e7b3b
luongtphu <luongtphu@gmail.com>
2014-01-10 20:53:52 +0700

    Delete _test folder

--- _test/gen - Thrift.Test.bat
+++
@@ -1,9 +0,0 @@
-@echo -----------------------------------------
-@echo Gen trialtest.thrift
-@echo If you did not see thrift.exe. Please Using VS2010 to recompile project
-@echo -----------------------------------------
-
-@..\bin\thrift --gen delphi7 Thrift.Test.thrift
-
-@echo Press any key to exit
-@pause
--- _test/gen.bat
+++
@@ -1,9 +0,0 @@
-@echo -----------------------------------------
-@echo Gen trialtest.thrift
-@echo If you did not see thrift.exe. Please Using VS2010 to recompile project
-@echo -----------------------------------------
-
-@..\bin\thrift --gen delphi7 trialtest.thrift
-
-@echo Press any key to exit
-@pause
